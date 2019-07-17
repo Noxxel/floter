d3c883be75e778c77e4e42cfac5f1a787b38e449
Flo <ffallenbuechel@aol.com>
2019-07-17 05:54:58 +0200

    remote debugging sucks

--- high_res_gan_v3/train.py
+++ high_res_gan_v3/train.py
@@ -231,6 +231,7 @@ if __name__ == '__main__':
         fixed_noise = torch.tensor([Mset[i].numpy() for i in range(1337,1337+opt.batchSize)], dtype=torch.float32).unsqueeze(2).unsqueeze(2).to(device)
     elif opt.conv:
         feature_maps = [conv.convolve(Mset[i][0].to(device).unsqueeze(0)).detach().cpu().numpy() for i in range(1337,1337+opt.batchSize)]
+        feature_maps = torch.tensor(feature_maps)
         print(feature_maps.shape)
         rand_index = np.random.randint(0, n_time_steps, size=opt.batchSize)
         #fixed_noise = 
