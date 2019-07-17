5903e236480769a88b5e730a2e789d63d1e426c6
Flo <ffallenbuechel@aol.com>
2019-07-17 06:09:10 +0200

    wanna go to sleep soon

--- high_res_gan_v3/train.py
+++ high_res_gan_v3/train.py
@@ -230,7 +230,7 @@ if __name__ == '__main__':
     elif opt.mel:
         fixed_noise = torch.tensor([Mset[i].numpy() for i in range(1337,1337+opt.batchSize)], dtype=torch.float32).unsqueeze(2).unsqueeze(2).to(device)
     elif opt.conv:
-        feature_maps = torch.tensor([conv.convolve(Mset[i][0].to(device).unsqueeze(0)).detach().squeeze() for i in range(1337,1337+opt.batchSize)])
+        feature_maps = torch.tensor([conv.convolve(Mset[i][0].to(device).unsqueeze(0)).detach().squeeze().cpu().numpy() for i in range(1337,1337+opt.batchSize)])
         feature_maps = feature_maps.reshape(feature_maps.shape[0], feature_maps.shape[2], feature_maps.shape[1])
         print(feature_maps.shape)
         rand_index = np.random.randint(0, n_time_steps, size=opt.batchSize)
