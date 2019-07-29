ad1587abb1c1856609035e9e9f470ef0f707e478
Peter Huegel <huegelp@gmail.com>
2019-07-29 20:25:50 +0200

    comments fixed

--- high_res_gan_v3/dcgan.py
+++ high_res_gan_v3/dcgan.py
@@ -64,35 +64,35 @@ class Discriminator(nn.Module):
             # input is (nc) x 512 x 512
             nn.Conv2d(in_channels=nc, out_channels=ndf, kernel_size=4, stride=2, padding=1, bias=False),
             nn.LeakyReLU(0.2, inplace=True),
-            # state size. (ndf) x 128 x 128
+            # state size. (ndf) x 256 x 256
             nn.Conv2d(in_channels=ndf, out_channels=2*ndf, kernel_size=4, stride=2, padding=1, bias=False),
             nn.BatchNorm2d(2*ndf),
             nn.LeakyReLU(0.2, inplace=True),
-            # state size. (ndf*2) x 64 x 64
+            # state size. (ndf*2) x 128 x 128
             nn.Conv2d(in_channels=2*ndf, out_channels=4*ndf, kernel_size=4, stride=2, padding=1, bias=False),
             nn.BatchNorm2d(4*ndf),
             nn.LeakyReLU(0.2, inplace=True),
-            # state size. (ndf*4) x 32 x 32
+            # state size. (ndf*4) x 64 x 64
             nn.Conv2d(in_channels=4*ndf, out_channels=6*ndf, kernel_size=4, stride=2, padding=1, bias=False),
             nn.BatchNorm2d(6*ndf),
             nn.LeakyReLU(0.2, inplace=True),
-            # state size. (ndf*6) x 16 x 16
+            # state size. (ndf*6) x 32 x 32
             nn.Conv2d(in_channels=6*ndf, out_channels=8*ndf, kernel_size=4, stride=2, padding=1, bias=False),
             nn.BatchNorm2d(8*ndf),
             nn.LeakyReLU(0.2, inplace=True),
-            # state size. (ndf*8) x 8 x 8
+            # state size. (ndf*8) x 16 x 16
             nn.Conv2d(in_channels=8*ndf, out_channels=10*ndf, kernel_size=4, stride=2, padding=1, bias=False),
             nn.BatchNorm2d(10*ndf),
             nn.LeakyReLU(0.2, inplace=True),
-            # state size. (ndf*10) x 4 x 4
+            # state size. (ndf*10) x 8 x 8
             nn.Conv2d(in_channels=10*ndf, out_channels=12*ndf, kernel_size=4, stride=2, padding=1, bias=False),
             nn.BatchNorm2d(12*ndf),
             nn.LeakyReLU(0.2, inplace=True),
-            # state size. (ndf*12) x 2 x 2
+            # state size. (ndf*12) x 4 x 4
             nn.Conv2d(in_channels=12*ndf, out_channels=14*ndf, kernel_size=4, stride=2, padding=1, bias=False),
             nn.BatchNorm2d(14*ndf),
             nn.LeakyReLU(0.2, inplace=True),
-            # state size. (ndf*14) x 1 x 1
+            # state size. (ndf*14) x 2 x 2
             nn.Conv2d(in_channels=14*ndf, out_channels=1, kernel_size=2, stride=2, padding=0, bias=False),
             # nn.AdaptiveAvgPool2d((1,1)),
             # batchsize x 1 x 1 x 1
