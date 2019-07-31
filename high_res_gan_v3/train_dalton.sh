51f569287a56de8f2d90071709911ab2f7635666
ffallenb <f.fallenbuechel@stud.uni-heidelberg.de>
2019-07-31 23:15:33 +0200

    Mini wini

--- high_res_gan_v3/train_dalton.sh
+++ high_res_gan_v3/train_dalton.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-python train.py --cuda --manualSeed=1337 --batchSize=8 --workers=4 --nz=32 --ngf=64 --ndf=32 --conv --l2size=32 --lr=0.00001
+python train.py --cuda --manualSeed=3142 --batchSize=8 --workers=4 --nz=32 --ngf=64 --ndf=32 --conv --l2size=32 --lr=0.000005
 
